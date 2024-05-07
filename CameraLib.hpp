fb6c09b5f8f916fd5fca63c105e8013f35b79866
Yusuf Averroes Sungkar <yusuf.averoes@gmail.com>
2024-05-07 10:31:55 +0700

    working on dry run

---
+++ CameraLib.hpp
@@ -0,0 +1,33 @@
+#include "CameraLib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+// Include other necessary headers...
+
+// Implement member functions here
+Camera::Camera() {
+    // Constructor implementation
+}
+
+Camera::~Camera() {
+    // Destructor implementation
+}
+
+int Camera::init() {
+    // Implementation of init function
+}
+
+int Camera::DeInitResources() {
+    // Implementation of DeInitResources function
+}
+
+cv::Mat Camera::getImage() {
+    // Implementation of getImage function
+}
+
+char* Camera::getString() {
+    // Implementation of getString function
+}
